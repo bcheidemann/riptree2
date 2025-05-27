1807e6f45481e419fd6119cdf6276832fb167276
Ben Heidemann <ben@heidemann.dev>
2025-05-27 22:30:47 +0100

    feat: initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,121 @@
-fn main() {
-    println!("Hello, world!");
+use std::{cmp::Ordering, fs::DirEntry, io::Write, path::PathBuf};
+
+use anyhow::Context as _;
+
+#[derive(Default)]
+struct TreeStats {
+    dirs: usize,
+    files: usize,
+}
+
+struct Tree {
+    sorter: fn(&DirEntry, &DirEntry) -> Ordering,
+    prefix: String,
+    root: PathBuf,
+}
+
+impl Tree {
+    fn enter_dir(&self, dir: &DirEntry, is_last: bool) -> Self {
+        let new_prefix = if is_last { "    " } else { "│   " };
+        Tree {
+            sorter: self.sorter,
+            prefix: format!("{}{}", self.prefix, new_prefix),
+            root: dir.path(),
+        }
+    }
+
+    #[inline]
+    fn write_entry(
+        &self,
+        w: &mut impl Write,
+        entry: &DirEntry,
+        is_last: bool,
+        stats: &mut TreeStats,
+    ) -> anyhow::Result<()> {
+        let result = if is_last {
+            writeln!(
+                w,
+                "{}└── {}",
+                self.prefix,
+                entry.file_name().to_string_lossy(),
+            )
+        } else {
+            writeln!(
+                w,
+                "{}├── {}",
+                self.prefix,
+                entry.file_name().to_string_lossy(),
+            )
+        };
+        result.context("Failed to write entry")?;
+
+        if entry.file_type().unwrap().is_dir() {
+            stats.dirs += 1;
+            self.enter_dir(entry, is_last).write(w, stats)?;
+        } else {
+            stats.files += 1;
+        }
+
+        Ok(())
+    }
+
+    fn write(&self, w: &mut impl Write, stats: &mut TreeStats) -> anyhow::Result<()> {
+        let mut entries = std::fs::read_dir(&self.root)
+            .context("Failed to read directory")?
+            .collect::<Vec<_>>();
+
+        entries.sort_by(|a, b| match (a, b) {
+            (&Ok(ref a), &Ok(ref b)) => default_sorter(a, b),
+            (&Err(_), &Err(_)) => Ordering::Equal,
+            (&Ok(_), &Err(_)) => Ordering::Greater,
+            (&Err(_), &Ok(_)) => Ordering::Less,
+        });
+
+        if let Some((last_entry, leading_entries)) = entries.split_last() {
+            for entry in leading_entries.iter() {
+                let entry = entry.as_ref().unwrap();
+                self.write_entry(w, entry, false, stats)?;
+            }
+            self.write_entry(w, &last_entry.as_ref().unwrap(), true, stats)?;
+        }
+
+        Ok(())
+    }
+
+    fn print(&self, stats: &mut TreeStats) -> anyhow::Result<()> {
+        let mut writer = std::io::stdout();
+        self.write(&mut writer, stats)
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let root = ".";
+
+    let mut stats = TreeStats::default();
+    let tree = Tree {
+        sorter: default_sorter,
+        prefix: "".to_string(),
+        root: root.into(),
+    };
+
+    println!("{root}");
+    tree.print(&mut stats)?;
+    println!("");
+    println!("{} directories, {} files", stats.dirs + 1, stats.files);
+
+    Ok(())
+}
+
+fn default_sorter(a: &DirEntry, b: &DirEntry) -> Ordering {
+    return a.file_name().cmp(&b.file_name());
+
+    // Folders last
+    // match (
+    //     a.file_type().unwrap().is_dir(),
+    //     b.file_type().unwrap().is_dir(),
+    // ) {
+    //     (true, false) => Ordering::Greater,
+    //     (false, true) => Ordering::Less,
+    //     _ => a.file_name().cmp(&b.file_name()),
+    // }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,10 @@
-use std::{fs::File, io::BufReader, path::Path, process::Command};
+use std::{
+    fs::File,
+    io::BufReader,
+    path::{Path, PathBuf},
+    process::Command,
+};
+use uuid::Uuid;
 
 use assert_cmd::{assert::OutputAssertExt as _, cargo::CommandCargoExt};
 use fixtures::fixtures;
@@ -6,8 +12,8 @@ use serde::Deserialize;
 
 #[derive(Deserialize)]
 struct TestDescription {
-    cwd: String,
     args: Vec<String>,
+    create_dirs: Option<Vec<PathBuf>>,
 }
 
 #[fixtures(["tests/fixtures/conformance/*"])]
@@ -18,11 +24,28 @@ fn test(dir: &Path) {
         let reader = BufReader::new(file);
         serde_json::from_reader(reader).unwrap()
     };
-    let working_dir = dir.join(test_description.cwd);
+    let dir_template = dir.join("dir_template");
+    let temp_working_dir = std::env::temp_dir().join(Uuid::new_v4().to_string());
+
+    let errors = copy_dir::copy_dir(&dir_template, &temp_working_dir).unwrap();
+    if !errors.is_empty() {
+        let errors = errors
+            .iter()
+            .enumerate()
+            .map(|(idx, error)| format!("[{idx}] {error}"))
+            .collect::<Vec<_>>()
+            .join("\n");
+        panic!("Errors:\n\n{errors}");
+    }
+
+    for dir in test_description.create_dirs.unwrap_or(vec![]) {
+        let dir = temp_working_dir.join(dir);
+        std::fs::create_dir_all(dir).unwrap();
+    }
 
     let reference_binary = std::env::var_os("TREE_REFERENCE_BIN").unwrap_or("tree".into());
     let reference_output = Command::new(reference_binary)
-        .current_dir(&working_dir)
+        .current_dir(&temp_working_dir)
         .args(&test_description.args)
         .output()
         .unwrap();
@@ -30,9 +53,17 @@ fn test(dir: &Path) {
     let reference_stdout = String::from_utf8(reference_output.stdout).unwrap();
     let reference_stderr = String::from_utf8(reference_output.stderr).unwrap();
 
+    let code_snapshot_path = dir.join("code.snap");
+    let stdout_snapshot_path = dir.join("stdout.snap");
+    let stderr_snapshot_path = dir.join("stderr.snap");
+
+    std::fs::write(code_snapshot_path, format!("{reference_code}")).unwrap();
+    std::fs::write(stdout_snapshot_path, format!("{reference_stdout}")).unwrap();
+    std::fs::write(stderr_snapshot_path, format!("{reference_stderr}")).unwrap();
+
     Command::cargo_bin(env!("CARGO_PKG_NAME"))
         .unwrap()
-        .current_dir(&working_dir)
+        .current_dir(&temp_working_dir)
         .args(&test_description.args)
         .assert()
         .code(reference_code)
