a2345c22ba3fd20deb4f13629afa6608d70c976c
Ben Heidemann <ben@heidemann.dev>
2025-06-28 13:28:31 +0100

    test: benchmark against reference implementation

--- benches/cli.rs
+++ benches/cli.rs
@@ -1,4 +1,5 @@
 use std::{
+    ffi::OsStr,
     fs::File,
     io::BufReader,
     path::Path,
@@ -6,7 +7,10 @@ use std::{
 };
 
 use assert_cmd::assert::OutputAssertExt;
-use criterion::{Criterion, criterion_group, criterion_main};
+use criterion::{
+    BenchmarkGroup, BenchmarkId, Criterion, criterion_group, criterion_main,
+    measurement::Measurement,
+};
 use serde::Deserialize;
 use test_utils::{TestWorkingDir, snapshot::assert_snapshot};
 
@@ -16,50 +20,53 @@ struct TestDescription {
     args: Vec<String>,
 }
 
-fn run_cli_benchmark(c: &mut Criterion, test_name: &str) {
-    let test_dir = format!("tests/fixtures/bench/{test_name}");
-    let test_dir = Path::new(&test_dir);
+fn run_cli_benchmark<M: Measurement>(
+    c: &mut BenchmarkGroup<M>,
+    test_name: &str,
+    binary: &OsStr,
+    args: &Vec<&str>,
+    benchmark_id: BenchmarkId,
+    snapshot_name: &str,
+) {
+    if std::env::var_os("SKIP_BENCH_ASSERTIONS").is_none() {
+        let test_dir = format!("tests/fixtures/bench/{test_name}");
+        let test_dir = Path::new(&test_dir);
 
-    Command::new("cargo")
-        .arg("build")
-        .arg("--release")
-        .assert()
-        .code(0);
+        let test_description: TestDescription = {
+            let path = test_dir.join("test.json");
+            let file = File::open(path).unwrap();
+            let reader = BufReader::new(file);
+            serde_json::from_reader(reader).unwrap()
+        };
 
-    let release_bin = std::env::current_dir().unwrap().join("target/release/rt");
+        eprintln!("[TEST DESCRIPTION]\n{}\n\n", test_description.description);
 
-    let test_description: TestDescription = {
-        let path = test_dir.join("test.json");
-        let file = File::open(path).unwrap();
-        let reader = BufReader::new(file);
-        serde_json::from_reader(reader).unwrap()
-    };
+        let test_working_dir = TestWorkingDir::new(test_dir);
 
-    eprintln!("[TEST DESCRIPTION]\n{}\n\n", test_description.description);
+        let sut_output = Command::new(binary)
+            .current_dir(&test_working_dir)
+            .args(args)
+            .args(&test_description.args)
+            .output()
+            .unwrap();
+        let sut_code = sut_output.status.code().unwrap().to_string();
+        let sut_stdout = String::from_utf8(sut_output.stdout).unwrap();
+        let sut_stderr = String::from_utf8(sut_output.stderr).unwrap();
 
-    let test_working_dir = TestWorkingDir::new(test_dir);
+        let code_snapshot_path = test_dir.join(format!("code.{snapshot_name}.snap"));
+        let stdout_snapshot_path = test_dir.join(format!("stdout.{snapshot_name}.snap"));
+        let stderr_snapshot_path = test_dir.join(format!("stderr.{snapshot_name}.snap"));
 
-    let sut_output = Command::new(&release_bin)
-        .current_dir(&test_working_dir)
-        .args(&test_description.args)
-        .output()
-        .unwrap();
-    let sut_code = sut_output.status.code().unwrap().to_string();
-    let sut_stdout = String::from_utf8(sut_output.stdout).unwrap();
-    let sut_stderr = String::from_utf8(sut_output.stderr).unwrap();
+        assert_snapshot(code_snapshot_path, sut_code);
+        assert_snapshot(stdout_snapshot_path, sut_stdout);
+        assert_snapshot(stderr_snapshot_path, sut_stderr);
+    }
 
-    let code_snapshot_path = test_dir.join("code.snap");
-    let stdout_snapshot_path = test_dir.join("stdout.snap");
-    let stderr_snapshot_path = test_dir.join("stderr.snap");
-
-    assert_snapshot(code_snapshot_path, sut_code);
-    assert_snapshot(stdout_snapshot_path, sut_stdout);
-    assert_snapshot(stderr_snapshot_path, sut_stderr);
-
-    c.bench_function(&test_name, |b| {
+    c.bench_function(benchmark_id, |b| {
         b.iter(|| {
-            Command::new(&release_bin)
+            Command::new(binary)
                 .current_dir(&test_working_dir)
+                .args(args)
                 .args(&test_description.args)
                 .stdout(Stdio::null())
                 .status()
@@ -68,10 +75,79 @@ fn run_cli_benchmark(c: &mut Criterion, test_name: &str) {
     });
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    run_cli_benchmark(c, "cli_all_file_types");
-    run_cli_benchmark(c, "cli_nested_dirs");
+fn criterion_benchmark_riptree2<M: Measurement>(c: &mut BenchmarkGroup<M>, test_name: &str) {
+    Command::new("cargo")
+        .arg("build")
+        .arg("--release")
+        .assert()
+        .code(0);
+
+    let release_bin = std::env::current_dir()
+        .unwrap()
+        .join("target/release/rt")
+        .into_os_string();
+    run_cli_benchmark(
+        c,
+        test_name,
+        &release_bin,
+        &vec![],
+        BenchmarkId::from_parameter("riptree2"),
+        "riptree2",
+    );
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn criterion_benchmark_riptree2_compat<M: Measurement>(c: &mut BenchmarkGroup<M>, test_name: &str) {
+    Command::new("cargo")
+        .arg("build")
+        .arg("--release")
+        .assert()
+        .code(0);
+
+    let release_bin = std::env::current_dir()
+        .unwrap()
+        .join("target/release/rt")
+        .into_os_string();
+    run_cli_benchmark(
+        c,
+        test_name,
+        &release_bin,
+        &vec!["--compat"],
+        BenchmarkId::from_parameter("riptree2/compat"),
+        "riptree2_compat",
+    );
+}
+
+fn criterion_benchmark_reference<M: Measurement>(c: &mut BenchmarkGroup<M>, test_name: &str) {
+    let release_bin = std::env::var_os("TREE_CMD").unwrap_or("tree".into());
+    run_cli_benchmark(
+        c,
+        test_name,
+        &release_bin,
+        &vec![],
+        BenchmarkId::from_parameter("reference"),
+        "reference",
+    );
+}
+
+fn bench_cli_all_file_types(c: &mut Criterion) {
+    let test_name = "cli_all_file_types";
+    let mut group = c.benchmark_group(test_name);
+    criterion_benchmark_riptree2(&mut group, test_name);
+    criterion_benchmark_riptree2_compat(&mut group, test_name);
+    criterion_benchmark_reference(&mut group, test_name);
+}
+
+fn bench_cli_nested_dirs(c: &mut Criterion) {
+    let test_name = "cli_nested_dirs";
+    let mut group = c.benchmark_group(test_name);
+    criterion_benchmark_riptree2(&mut group, test_name);
+    criterion_benchmark_riptree2_compat(&mut group, test_name);
+    criterion_benchmark_reference(&mut group, test_name);
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default();
+    targets = bench_cli_all_file_types, bench_cli_nested_dirs
+}
 criterion_main!(benches);
